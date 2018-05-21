7ee7fbc2ff445858ec706c6f95ea1e0bcd103c20
Charly Chávez Ordóñez <nekperu15739@gmail.com>
2018-05-21 15:59:50 +0200

    removed unnecessary mock implementation Added refactor service test

--- src/test/java/org/np9/reactive/HardCodeServiceTest.java
+++ src/test/java/org/np9/reactive/HardCodeServiceTest.java
@@ -1,26 +1,46 @@
 package org.np9.reactive;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.np9.reactive.dto.Movie;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 /**
  * @author nekperu15739
  */
+@RunWith(MockitoJUnitRunner.class)
 public class HardCodeServiceTest {
 
-    private final ReactiveMovieService service = new MockReactiveMovieMovieService();
+    public static final String ID = "id";
+
+    @Mock
+    private ReactiveMovieService movieService;
+
+    @Mock
+    private Mono<Movie> monoOfMovie;
+
+    @Mock
+    private Flux<Movie> fluxOfMovies;
 
     @Test
     public void all() {
-        final Flux<Movie> movies = service.all();
-        movies.subscribe(System.out::println);
+        when(movieService.all()).thenReturn(fluxOfMovies);
+        StepVerifier.create(movieService.all())
+                .assertNext(movie -> assertThat(movie).isNotNull());
     }
 
     @Test
     public void byId() {
-        final Mono<Movie> movie = service.byId("id");
-        movie.subscribe(System.out::println);
+        when(movieService.byId(ID)).thenReturn(monoOfMovie);
+        StepVerifier.create(movieService.byId(ID))
+                .assertNext(movie -> assertThat(movie).isNotNull());
     }
+
 }
